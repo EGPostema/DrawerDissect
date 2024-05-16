fb878e9898d8100ac816360957156c6679ad6fe9
EGPostema <142446286+EGPostema@users.noreply.github.com>
2024-05-16 18:36:48 -0500

    adds _1000 extension

--- resize_trays.py
+++ resize_trays.py
@@ -6,13 +6,15 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def resize_tray_images(input_dir, output_dir, new_width=1000):
     for filename in os.listdir(input_dir):
-        input_path = os.path.join(input_dir, filename)
-        output_path = os.path.join(output_dir, filename)
-        with Image.open(input_path) as img:
-            new_height = int((new_width / img.width) * img.height)
-            resized_img = img.resize((new_width, new_height), Image.LANCZOS)
-            resized_img.save(output_path)
+        if filename.endswith('.jpg'):
+            input_path = os.path.join(input_dir, filename)
+            output_path = os.path.join(output_dir, filename.replace('.jpg', '_1000.jpg'))
+            with Image.open(input_path) as img:
+                new_height = int((new_width / img.width) * img.height)
+                resized_img = img.resize((new_width, new_height), Image.LANCZOS)
+                resized_img.save(output_path)
 
 if __name__ == '__main__':
-    resize_tray_images('data/drawers/trays', 'data/drawers/resized_trays')
+    resize_tray_images('coloroptera/drawers/trays', 'coloroptera/drawers/resized_trays')
+
 
