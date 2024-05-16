5578a0c726d7a5a3fbafee0f70937f336e89fe4c
EGPostema <142446286+EGPostema@users.noreply.github.com>
2024-05-16 18:35:57 -0500

    adds _1000 extension

--- resize_drawer.py
+++ resize_drawer.py
@@ -6,13 +6,14 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def resize_drawer_images(input_dir, output_dir, new_width=1000):
     for filename in os.listdir(input_dir):
-        input_path = os.path.join(input_dir, filename)
-        output_path = os.path.join(output_dir, filename)
-        with Image.open(input_path) as img:
-            new_height = int((new_width / img.width) * img.height)
-            resized_img = img.resize((new_width, new_height), Image.LANCZOS)
-            resized_img.save(output_path)
+        if filename.endswith('.jpg'):
+            input_path = os.path.join(input_dir, filename)
+            output_path = os.path.join(output_dir, filename.replace('.jpg', '_1000.jpg'))
+            with Image.open(input_path) as img:
+                new_height = int((new_width / img.width) * img.height)
+                resized_img = img.resize((new_width, new_height), Image.LANCZOS)
+                resized_img.save(output_path)
 
 if __name__ == '__main__':
-    resize_drawer_images('data/drawers/fullsize', 'data/drawers/resized')
+    resize_drawer_images('coloroptera/drawers/fullsize', 'coloroptera/drawers/resized')
 
