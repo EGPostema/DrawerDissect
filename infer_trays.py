224eda6bd58763a8372efedbfec8f6d79eab153e
EGPostema <142446286+EGPostema@users.noreply.github.com>
2024-05-16 18:37:30 -0500

    Update infer_trays.py

--- infer_trays.py
+++ infer_trays.py
@@ -2,21 +2,21 @@ import os
 import json
 from roboflow import Roboflow
 
-def infer_tray_images(input_dir, api_key='YOUR_API_KEY', model_endpoint='YOUR_MODEL_ENDPOINT', version=1, confidence=50, overlap=50):
+def infer_tray_images(input_dir, output_dir, api_key='YOUR_API_KEY', model_endpoint='YOUR_MODEL_ENDPOINT', version=1, confidence=50, overlap=50):
+    os.makedirs(output_dir, exist_ok=True)
     rf = Roboflow(api_key=api_key)
     project = rf.workspace().project(model_endpoint)
     model = project.version(version).model
 
     for root, _, files in os.walk(input_dir):
         for file in files:
-            if file.endswith(".jpg"):
+            if file.endswith('_1000.jpg'):
                 file_path = os.path.join(root, file)
                 prediction = model.predict(file_path, confidence=confidence, overlap=overlap).json()
-                # Save the prediction as a JSON file
-                json_path = file_path.replace('.jpg', '.json')
+                json_path = os.path.join(output_dir, file.replace('.jpg', '.json'))
                 with open(json_path, 'w') as json_file:
                     json.dump(prediction, json_file)
 
 if __name__ == '__main__':
-    infer_tray_images('data/drawers/resized_trays')
+    infer_tray_images('coloroptera/drawers/resized_trays', 'coloroptera/drawers/resized_trays/coordinates')
 
