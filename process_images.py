73e022d65e8f532b4864202fdb4eacdbdc6011b6
EGPostema <142446286+EGPostema@users.noreply.github.com>
2024-06-21 11:47:44 -0500

    streamlined user inputs

--- process_images.py
+++ process_images.py
@@ -8,48 +8,39 @@ import roboflow
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
 # User inputs for API key and model details **MAKE SURE TO MODIFY THIS!**
-API_KEY = 'YOUR_API_HERE'
-WORKSPACE = 'YOUR_WORKSPACE_HERE'
+API_KEY = 'API-KEY-HERE'
+WORKSPACE = 'WORKSPACE-ID-HERE'
 
 # Initialize Roboflow
 rf = roboflow.Roboflow(api_key=API_KEY)
 workspace = rf.workspace(WORKSPACE)
 
-# User inputs for roboflow model that seperates TRAYS from DRAWERS **MAKE SURE TO MODIFY THIS!**
-DRAWER_MODEL_ENDPOINT = 'TRAY_SEPERATING_MODEL_HERE'
+# User inputs for roboflow models **MAKE SURE TO MODIFY THESE!**
+DRAWER_MODEL_ENDPOINT = 'TRAY-SEPERATING-MODEL-HERE'
 DRAWER_MODEL_VERSION = 1  # Adjust the version as needed!
-
-# User inputs for roboflow model that seperates SPECIMENS from TRAYS **MAKE SURE TO MODIFY THIS!**
-TRAY_MODEL_ENDPOINT = 'SPECIMEN_SEPERATING_MODEL_HERE'
+TRAY_MODEL_ENDPOINT = 'SPECIMEN-SEPERATING-MODEL-HERE'
 TRAY_MODEL_VERSION = 1  # Adjust the version as needed
-
-# User inputs for roboflow model that detects labels **MAKE SURE TO MODIFY THIS!**
-LABEL_MODEL_ENDPOINT = 'labelfinder'
+LABEL_MODEL_ENDPOINT = 'LABEL-TRANSCRIPTION-MODEL-HERE'
 LABEL_MODEL_VERSION = 1  # Adjust the version as needed!
 
 # Define directories
-fullsize_dir = 'drawers/fullsize'
-resized_dir = 'drawers/resized'
-coordinates_dir = os.path.join(resized_dir, 'coordinates')
-trays_dir = 'drawers/trays'
-resized_trays_dir = 'drawers/resized_trays'
-resized_trays_coordinates_dir = os.path.join(resized_trays_dir, 'coordinates')
-specimens_dir = 'drawers/specimens'
+directories = {
+    'fullsize': 'drawers/fullsize',
+    'resized': 'drawers/resized',
+    'coordinates': 'drawers/resized/coordinates',
+    'trays': 'drawers/trays',
+    'resized_trays': 'drawers/resized_trays',
+    'resized_trays_coordinates': 'drawers/resized_trays/coordinates',
+    'specimens': 'drawers/specimens'
+}
 
 # Ensure necessary directories exist
-directories = [fullsize_dir, resized_dir, coordinates_dir, trays_dir, resized_trays_dir, resized_trays_coordinates_dir, specimens_dir]
-all_exist = True
-for directory in directories:
+for name, directory in directories.items():
     if not os.path.exists(directory):
         print(f"Creating directory: {directory}")
         os.makedirs(directory, exist_ok=True)
-        all_exist = False
-
-if all_exist:
-    print("All directories exist.")
 
 # Import functions from functions folder
-
 from functions.resize_drawer import resize_drawer_images
 from functions.infer_drawers import infer_drawers
 from functions.crop_trays import crop_trays_from_fullsize
@@ -57,6 +48,10 @@ from functions.resize_trays import resize_tray_images
 from functions.label_transcription import transcribe_labels_and_ids
 from functions.infer_trays import infer_tray_images
 from functions.crop_specimens import crop_specimens_from_trays
+
+def load_roboflow_workspace_and_project():
+    rf = roboflow.Roboflow(api_key=API_KEY)
+    workspace = rf.workspace(WORKSPACE)
 
 def main():
     parser = argparse.ArgumentParser(description="Process images with specified confidence and overlap.")
@@ -73,53 +68,55 @@ def main():
 
     # Function to run all steps sequentially
     def run_all_steps():
-        resize_drawer_images(fullsize_dir, resized_dir)
-        print(f"Resized drawers saved in {resized_dir}")
+        resize_drawer_images(directories['fullsize'], directories['resized'])
+        print(f"Resized drawers saved in {directories['resized']}")
 
-        infer_drawers(resized_dir, coordinates_dir, API_KEY, DRAWER_MODEL_ENDPOINT, DRAWER_MODEL_VERSION, confidence=args.drawer_confidence, overlap=args.drawer_overlap)
-        print(f"Tray bounding boxes saved in {coordinates_dir}")
+        infer_drawers(directories['resized'], directories['coordinates'], API_KEY, DRAWER_MODEL_ENDPOINT, DRAWER_MODEL_VERSION, confidence=args.drawer_confidence, overlap=args.drawer_overlap)
+        print(f"Tray bounding boxes saved in {directories['coordinates']}")
 
-        crop_trays_from_fullsize(fullsize_dir, resized_dir, trays_dir)
-        print(f"Cropped trays saved in {trays_dir}")
+        crop_trays_from_fullsize(directories['fullsize'], directories['resized'], directories['trays'])
+        print(f"Cropped trays saved in {directories['trays']}")
 
-        resize_tray_images(trays_dir, resized_trays_dir)
-        print(f"Resized tray images saved in {resized_trays_dir}")
+        resize_tray_images(directories['trays'], directories['resized_trays'])
+        print(f"Resized tray images saved in {directories['resized_trays']}")
 
-        transcribe_labels_and_ids(resized_trays_dir, API_KEY, LABEL_MODEL_ENDPOINT, LABEL_MODEL_VERSION, confidence=args.label_confidence, overlap=args.label_overlap)
-        print(f"Labels transcribed and saved in {os.path.join(resized_trays_dir, 'label_data')}")
+        transcribe_labels_and_ids(directories['resized_trays'], API_KEY, LABEL_MODEL_ENDPOINT, LABEL_MODEL_VERSION, confidence=args.label_confidence, overlap=args.label_overlap)
+        print(f"Labels transcribed and saved in {os.path.join(directories['resized_trays'], 'label_data')}")
 
-        infer_tray_images(resized_trays_dir, resized_trays_coordinates_dir, API_KEY, TRAY_MODEL_ENDPOINT, TRAY_MODEL_VERSION, confidence=args.tray_confidence, overlap=args.tray_overlap)
-        print(f"Specimen bounding boxes for all trays saved in {resized_trays_coordinates_dir}")
+        infer_tray_images(directories['resized_trays'], directories['resized_trays_coordinates'], API_KEY, TRAY_MODEL_ENDPOINT, TRAY_MODEL_VERSION, confidence=args.tray_confidence, overlap=args.tray_overlap)
+        print(f"Specimen bounding boxes for all trays saved in {directories['resized_trays_coordinates']}")
 
-        crop_specimens_from_trays(trays_dir, resized_trays_dir, specimens_dir)
-        print(f"Cropped specimens saved in {specimens_dir}")
+        crop_specimens_from_trays(directories['trays'], directories['resized_trays'], directories['specimens'])
+        print(f"Cropped specimens saved in {directories['specimens']}")
 
     # Let's process some images!
     print("Starting the image processing pipeline...")
 
+    load_roboflow_workspace_and_project()
+
     if args.step == 'all':
         run_all_steps()
     elif args.step == 'resize_drawers':
-        resize_drawer_images(fullsize_dir, resized_dir)
-        print(f"Resized drawers saved in {resized_dir}")
+        resize_drawer_images(directories['fullsize'], directories['resized'])
+        print(f"Resized drawers saved in {directories['resized']}")
     elif args.step == 'infer_drawers':
-        infer_drawers(resized_dir, coordinates_dir, API_KEY, DRAWER_MODEL_ENDPOINT, DRAWER_MODEL_VERSION, confidence=args.drawer_confidence, overlap=args.drawer_overlap)
-        print(f"Tray bounding boxes saved in {coordinates_dir}")
+        infer_drawers(directories['resized'], directories['coordinates'], API_KEY, DRAWER_MODEL_ENDPOINT, DRAWER_MODEL_VERSION, confidence=args.drawer_confidence, overlap=args.drawer_overlap)
+        print(f"Tray bounding boxes saved in {directories['coordinates']}")
     elif args.step == 'crop_trays':
-        crop_trays_from_fullsize(fullsize_dir, resized_dir, trays_dir)
-        print(f"Cropped trays saved in {trays_dir}")
+        crop_trays_from_fullsize(directories['fullsize'], directories['resized'], directories['trays'])
+        print(f"Cropped trays saved in {directories['trays']}")
     elif args.step == 'resize_trays':
-        resize_tray_images(trays_dir, resized_trays_dir)
-        print(f"Resized tray images saved in {resized_trays_dir}")
+        resize_tray_images(directories['trays'], directories['resized_trays'])
+        print(f"Resized tray images saved in {directories['resized_trays']}")
     elif args.step == 'transcribe_labels':
-        transcribe_labels_and_ids(resized_trays_dir, API_KEY, LABEL_MODEL_ENDPOINT, LABEL_MODEL_VERSION, confidence=args.label_confidence, overlap=args.label_overlap)
-        print(f"Labels transcribed and saved in {os.path.join(resized_trays_dir, 'label_data')}")
+        transcribe_labels_and_ids(directories['resized_trays'], API_KEY, LABEL_MODEL_ENDPOINT, LABEL_MODEL_VERSION, confidence=args.label_confidence, overlap=args.label_overlap)
+        print(f"Labels transcribed and saved in {os.path.join(directories['resized_trays'], 'label_data')}")
     elif args.step == 'infer_trays':
-        infer_tray_images(resized_trays_dir, resized_trays_coordinates_dir, API_KEY, TRAY_MODEL_ENDPOINT, TRAY_MODEL_VERSION, confidence=args.tray_confidence, overlap=args.tray_overlap)
-        print(f"Specimen bounding boxes for all trays saved in {resized_trays_coordinates_dir}")
+        infer_tray_images(directories['resized_trays'], directories['resized_trays_coordinates'], API_KEY, TRAY_MODEL_ENDPOINT, TRAY_MODEL_VERSION, confidence=args.tray_confidence, overlap=args.tray_overlap)
+        print(f"Specimen bounding boxes for all trays saved in {directories['resized_trays_coordinates']}")
     elif args.step == 'crop_specimens':
-        crop_specimens_from_trays(trays_dir, resized_trays_dir, specimens_dir)
-        print(f"Cropped specimens saved in {specimens_dir}")
+        crop_specimens_from_trays(directories['trays'], directories['resized_trays'], directories['specimens'])
+        print(f"Cropped specimens saved in {directories['specimens']}")
 
     print("Image processing pipeline completed.")
 
