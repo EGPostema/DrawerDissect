36f3cf781274b76b02fcd0a39b0a798b605f4461
EGPostema <142446286+EGPostema@users.noreply.github.com>
2025-03-19 15:08:04 -0500

    removed pd

--- functions/infer_pins.py
+++ functions/infer_pins.py
@@ -1,7 +1,6 @@
 import os
 import json
 import tempfile
-import pandas as pd
 import logging
 from PIL import Image
 from contextlib import contextmanager
@@ -11,7 +10,6 @@ from roboflow import Roboflow
 logging.getLogger('PIL').setLevel(logging.INFO)
 logging.getLogger('urllib3').setLevel(logging.WARNING)  # Silence urllib3 debug messages
 logging.getLogger('roboflow').setLevel(logging.WARNING)  # Silence Roboflow debug messages if any
-
 @contextmanager
 def temporary_jpg_if_needed(image_path):
     """Creates temporary JPG version if image is TIFF."""
@@ -30,13 +28,7 @@ def temporary_jpg_if_needed(image_path):
         finally:
             if os.path.exists(temp_file_path):
                 os.remove(temp_file_path)
-
 def infer_pins(input_dir, output_dir, csv_path, rf_instance, workspace_instance, model_endpoint, version, confidence=50):
-    # Load and filter the CSV data
-    data = pd.read_csv(csv_path)
-    filtered_data = data[data['mask_OK'] == 'Y']
-    valid_ids = set(filtered_data['full_id'])
-    
     # Initialize the Roboflow project and model
     project = workspace_instance.project(model_endpoint)
     model = project.version(version).model
@@ -44,7 +36,7 @@ def infer_pins(input_dir, output_dir, csv_path, rf_instance, workspace_instance,
     # Define valid extensions
     valid_extensions = ('.tif', '.tiff', '.png', '.jpg', '.jpeg')
     
-    for root, _, files in os.walk(input_dir):
+    for root, *, files in os.walk(input*dir):
         for file in files:
             # Check for valid extensions and '_masked' in the filename
             if not file.lower().endswith(valid_extensions) or '_masked' not in file:
@@ -53,10 +45,6 @@ def infer_pins(input_dir, output_dir, csv_path, rf_instance, workspace_instance,
             # Extract full_id by removing '_masked' and the file extension
             full_id = file.replace('_masked', '').rsplit('.', 1)[0]
             
-            # Skip files not in valid_ids
-            if full_id not in valid_ids:
-                continue
-                
             # Create output subfolder
             relative_path = os.path.relpath(root, input_dir)
             output_subfolder = os.path.join(output_dir, relative_path)
@@ -84,6 +72,5 @@ def infer_pins(input_dir, output_dir, csv_path, rf_instance, workspace_instance,
                 print(f"Processed {file}")  # Simplified progress message
             except Exception as e:
                 print(f"Error processing {file}: {e}")
-
 
 
